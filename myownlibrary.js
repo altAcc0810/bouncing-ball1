5a3f7e38e552d11a953e753f5715132c330e6bfe
Rajvigneshwar <80456252+Rajvigneshwar@users.noreply.github.com>
2021-06-07 19:14:15 +0530

    Add files via upload

---
+++ myownlibrary.js
@@ -0,0 +1,32 @@
+function isTouching(obj1,obj2){
+    if(obj1.x-obj2.x<obj1.width/2+obj2.width/2
+      &&obj2.x-obj1.x<obj1.width/2+obj2.width/2
+      &&obj1.y-obj2.y<obj1.height/2+obj2.height/2
+      &&obj2.y-obj1.y<obj1.height/2+obj2.height/2){
+  
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
+  function bounceOff(obj1,obj2){
+  
+    if(obj1.x-obj2.x<obj1.width/2+obj2.width/2
+      &&obj2.x-obj1.x<obj1.width/2+obj2.width/2)
+    
+      {
+  
+        
+        
+        
+        
+    }
+     if(obj1.y-obj2.y<obj1.height/2+obj2.height/2
+      &&obj2.y-obj1.y<obj1.height/2+obj2.height/2){ 
+    
+        obj1.velocityY = -10;
+        
+    }
+  }
